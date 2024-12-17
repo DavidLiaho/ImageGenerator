958a9fde1aa16e4f40f03aaf9c9f12d2253f8d4c
DavidLiaho <robodavli@gmail.com>
2024-12-17 12:02:18 +0200

    Fixed spinner

--- src/Components/SharedArea/Spinner/Spinner.tsx
+++ src/Components/SharedArea/Spinner/Spinner.tsx
@@ -4,6 +4,7 @@ import spinner from "../../../Assets/Images/spinner.gif";
 export function Spinner(): JSX.Element {
     return (
         <div className="Spinner">
+            <h1>Generating...</h1>
 			<img src={spinner} />
         </div>
     );
